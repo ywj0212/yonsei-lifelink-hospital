6e33b20e6123d791741e0da736a33389ad4fb569
유우진 <ywj02120212@gmail.com>
2023-11-12 03:08:18 +0900

    chore: Set Title and Meta Description of each route, Add .DS_Store to .gitignore

--- src/App.js
+++ src/App.js
@@ -27,20 +27,24 @@ function App() {
 
     switch (pathname) {
       case "/":
-        title = "";
-        metaDescription = "";
+        title = "Life-Link 메인페이지";
+        metaDescription = "Life-Link 병원 관리자 사이트 메인화면입니다.";
         break;
       case "/totalpage":
-        title = "";
-        metaDescription = "";
+        title = "Life-Link 종합상황판";
+        metaDescription = "Life-Link 병원 관리자 사이트의 병원 현황 페이지입니다.";
         break;
       case "/doctorpage":
-        title = "";
-        metaDescription = "";
+        title = "Life-Link 의료진 검색";
+        metaDescription = "Life-Link 병원 관리자 사이트의 의료진 검색 페이지입니다.";
         break;
       case "/hospitalpage":
-        title = "";
-        metaDescription = "";
+        title = "Life-Link 주변 응급실";
+        metaDescription = "Life-Link 병원 관리자 사이트의 주변 이송 가능한 응급실을 확인할 수 있는 페이지입니다.";
+        break;
+      default:
+        title = "Life-Link";
+        metaDescription = "응급 의료 종합 시스템 Life-Link의 병원용 관리자 사이트입니다.";
         break;
     }
 
